59f4f1a955c8d6279ab1b56e7b7c44da106d71e8
Łukasz Czarnołęcki <s7684@pjwstk.edu.pl>
2013-02-27 17:09:58 +0100

    Update Adwords/Conversion.php

--- Adwords/Conversion.php
+++ Adwords/Conversion.php
@@ -7,12 +7,33 @@ class Conversion
     private $id;
     private $label;
     private $value;
+    private $format;
+    private $color;
+    private $language;
 
-    public function __construct($id, $label, $value)
+    public function __construct($id, $label, $value, $format, $color, $language)
     {
         $this->id = $id;
         $this->label = $label;
         $this->value = $value;
+        $this->format = $format;
+        $this->color = $color;
+        $this->language = $language;
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    public function getFormat()
+    {
+        return $this->format;
+    }
+
+    public function getLanguage()
+    {
+        return $this->language;
     }
 
     public function getId()
